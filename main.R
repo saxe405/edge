63a6a5a558569e2d1f43f1795d5aa963e31e7da2
saxe405 <apurvsaxena16@gmail.com>
2018-08-28 14:53:32 +0200

    adding new files

---
+++ main.R
@@ -0,0 +1,2 @@
+# The aim is to find the stationary distribution
+source('system_definitions.R')
---
+++ matrix_definitions.R
@@ -0,0 +1,74 @@
+source('system_definitions.R')
+################################################################
+#returns F_i
+################################################################
+forward_matrix <-function(lambda,i,m)
+{
+  forward_matrix = matrix(0,nrow = m+1, ncol =m+1)
+  for(j in 1:(m+1) )
+  {
+    j0 = j-1 #there is no index 0 in R so everything is moved by 1 
+    val = 0;
+    for(n in 0:j)
+    {
+      val = val + prob_n_arrivals(n+i+m-j0)*prob_n_departures(j0,n);
+    }
+    forward_matrix[j,m+1] = val;
+  }
+  forward_matrix
+}
+
+################################################################
+# returns B_i
+################################################################
+backward_matrix <- function(lambda,i,m)
+{
+  backward_matrix = matrix(0,nrow= m+1, ncol =m+1)
+  for(j in 1:(m+1))
+  {
+    j0 = j-1
+    val = 0;
+    for(n in 0:m)
+    {
+      val = val + prob_n_arrivals(n-m-i+j0)*prob_n_departures(m,n);
+    }
+    backward_matrix[m+1,j] = val;
+  }
+  backward_matrix
+}
+################################################################## 
+# S_k matrix
+###################################################################
+S_matrix <- function(lambda,k,m)
+{
+  S_matrix = matrix(0,nrow=m+1, ncol = m+1);
+  for(n in 0:m)
+  {
+    S_matrix[m+1,m+1] = S_matrix[m+1,m+1] + prob_n_arrivals(n+k,lambda)*prob_n_departures(m,n)
+  }
+  S_matrix
+}
+
+#####################################################################
+# R0 matrix
+######################################################################
+
+R0_matrix <- function(lambda,m)
+{
+  R0_matrix = matrix(0,nrow=m+1,ncol=m+1)
+  for(j10 in 1:(m+1))
+  {
+    for(j20 in 1:(m+1))
+    {
+      j1 = j10-1;
+      j2 = j20-1
+      val = 0;
+      for(n in 0:j1)
+      {
+        val = val + prob_n_arrivals(n+j2-j1,lambda)*prob_n_departures(j1,n);
+      }
+      R0_matrix[j10,j20] = val;
+    }
+  }
+  R0_matrix
+}
